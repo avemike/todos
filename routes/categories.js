c5efb6b6108181930f105738a3c250d415beb448
avemike <fajny.michal.gajek@gmail.com>
2019-08-10 12:22:47 +0200

    category model, post and get for categories

---
+++ routes/categories.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const { categoryModel } = require('../models/categoryModel')
+const router = express.Router()
+
+// Get all todos
+router.get('/', (req, res) => {
+  categoryModel.find({}, (err, category) => {
+    res.send(category)
+  })
+})
+// Create todo
+router.post('/', async (req, res) => {
+  let category = new categoryModel({
+    name: req.body.name
+  })
+
+  category = await category.save()
+  res.send(category)
+})
