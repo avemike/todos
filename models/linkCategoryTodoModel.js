35bdd004508b19e13c164e34122472469abfad41
avemike <fajny.michal.gajek@gmail.com>
2019-08-11 20:06:37 +0200

    linkCategoryTodoModel

---
+++ models/linkCategoryTodoModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const linkCategoryTodoModel = mongoose.model('Todo-Category relation', new mongoose.Schema({
+    todo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Todo',
+        require: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        require: true
+    }
+}));
+
+exports.linkCategoryTodoModel = linkCategoryTodoModel;
