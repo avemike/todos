71b1e39e253290d6917076a481af3a9f97d013d6
avemike <fajny.michal.gajek@gmail.com>
2020-03-22 16:41:35 +0100

    webpack, react and express boilerplate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Index = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to shit!!!</h1>
+    </div>
+  );
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
